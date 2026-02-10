0ed87d85ced05bab721ce6243029ef8a87c1187b
DenisPuzko <st6785994@stud.duikt.edu.ua>
2026-02-10 13:44:21 +0200

    lb12 2.0

--- lb12/Lb121.cpp
+++ lb12/Lb121.cpp
@@ -2,21 +2,17 @@
 using namespace std;
 
 int main(void) {
-    // Оголошення масиву за зразком [cite: 27]
     int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
-    int n = sizeof(vector) / sizeof(vector[0]); // [cite: 28]
+    int n = sizeof(vector) / sizeof(vector[0]);
 
-    // --- Початок твого коду ---
-    int *ptr = vector; // Вказівник на початок масиву 
-    int min = *ptr;    // Припускаємо, що перший елемент найменший
+    int *ptr = vector;
+    int min = *ptr;
 
     for (int i = 1; i < n; i++) {
-        // Використовуємо арифметику вказівників замість індексації [cite: 23]
         if (*(ptr + i) < min) {
             min = *(ptr + i);
         }
     }
-    // --- Кінець твого коду ---
 
     cout << "Min element: " << min << endl;
     return 0; // [cite: 30]
